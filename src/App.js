808c2f6ea79dc13ba54b4a53edbf44a4d26b9ccb
Willem Verbuyst <willemverbuyst@gmail.com>
2020-07-11 07:16:22 +0200

    Set up reducer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import './App.css';
 
 const stateMachine = {
-  initial: { on: { next: 'loadingModel' } },
-  loadingModel: { on: { next: 'awaitingUpload' } },
-  awaitingUpload: { on: { next: 'ready' } },
-  ready: { on: { next: 'classifying' } },
-  classifying: { on: { next: 'complete' } },
-  complete: { on: { next: 'awaitingUpload' } },
+  initialState: 'initial',
+  states: {
+    initial: { on: { next: 'loadingModel' } },
+    loadingModel: { on: { next: 'awaitingUpload' } },
+    awaitingUpload: { on: { next: 'ready' } },
+    ready: { on: { next: 'classifying' } },
+    classifying: { on: { next: 'complete' } },
+    complete: { on: { next: 'awaitingUpload' } },
+  },
 };
 
+const reducer = (currentState, event) =>
+  stateMachine.states[currentState].on[event] || stateMachine.initialState;
+
 function App() {
-  return <div className="App">hello</div>;
+  const [state, dispatch] = useReducer(reducer, stateMachine.initialState);
+
+  return (
+    <div>
+      <button onClick={() => dispatch('next')}>{state}</button>
+    </div>
+  );
 }
 
 export default App;
