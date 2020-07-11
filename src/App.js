df6cb8c0d534eb0555d98177ea285a07e643c31b
Willem Verbuyst <willemverbuyst@gmail.com>
2020-07-11 07:25:57 +0200

    Create buttonProps for dynamic button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useState } from 'react';
+import * as mobilenet from '@tensorflow-models/mobilenet';
 import './App.css';
 
 const stateMachine = {
@@ -7,21 +8,38 @@ const stateMachine = {
     initial: { on: { next: 'loadingModel' } },
     loadingModel: { on: { next: 'awaitingUpload' } },
     awaitingUpload: { on: { next: 'ready' } },
-    ready: { on: { next: 'classifying' } },
+    ready: { on: { next: 'classifying' }, showImage: true },
     classifying: { on: { next: 'complete' } },
-    complete: { on: { next: 'awaitingUpload' } },
+    complete: { on: { next: 'awaitingUpload' }, showImage: true },
   },
 };
 
 const reducer = (currentState, event) =>
   stateMachine.states[currentState].on[event] || stateMachine.initialState;
+const [model, setModel] = useState(null);
 
 function App() {
   const [state, dispatch] = useReducer(reducer, stateMachine.initialState);
+  const next = () => dispatch('next');
+
+  const loadModel = async () => {
+    const mobilenetModel = await mobilenet.load();
+  };
+
+  const buttonProps = {
+    initial: { text: 'Load Model', action: () => {} },
+    loadingModel: { text: 'Loading model ...', action: () => {} },
+    awaitingUpload: { text: 'Upload photo', action: () => {} },
+    ready: { text: 'Identify', action: () => {} },
+    classifying: { text: 'Identifying', action: () => {} },
+    complete: { text: 'Reset', action: () => {} },
+  };
 
   return (
     <div>
-      <button onClick={() => dispatch('next')}>{state}</button>
+      <button onClick={buttonProps[state].action}>
+        {buttonProps[state].text}
+      </button>
     </div>
   );
 }
